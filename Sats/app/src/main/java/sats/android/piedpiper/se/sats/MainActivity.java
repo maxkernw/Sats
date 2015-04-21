64b88c2842b23b3556900a02b07f3983d1853989
Maximilian Kern <maxkernw@gmail.com>
2015-04-21 15:10:19 +0200

    style

--- Sats/app/src/main/java/sats/android/piedpiper/se/sats/MainActivity.java
+++ Sats/app/src/main/java/sats/android/piedpiper/se/sats/MainActivity.java
@@ -1,22 +1,28 @@
 package sats.android.piedpiper.se.sats;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.FrameLayout;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 
 
 public class MainActivity extends Activity
 {
-
+    ListView listView;
+    Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.min_traning);
+        listView = (ListView)findViewById(R.id.listan);
+
+        listView.setAdapter(new TestAdapter(this, new String[]
+                { "data1","data2","data3","data4","data5","data6","data7","data8" }));
+
 
     }
 
