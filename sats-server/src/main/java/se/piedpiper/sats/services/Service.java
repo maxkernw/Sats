8b6e31deaabd37c113d4dc22f424738748df0505
Stefan De Geer <sdgkeff@gmail.com>
2015-04-23 13:36:49 +0200

    jax-rs fungerar mycket bra

--- sats-server/src/main/java/se/piedpiper/sats/services/Service.java
+++ sats-server/src/main/java/se/piedpiper/sats/services/Service.java
@@ -1,5 +1,10 @@
 package se.piedpiper.sats.services;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -7,6 +12,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import se.piedpiper.sats.repositories.ActivityRepo;
 import se.piedpiper.sats.repositories.TypeRepo;
@@ -16,9 +22,21 @@ import se.piedpiper.sats.repositories.TypeRepo;
 public final class Service {
 
 	@GET
-	public Response getActivity(@QueryParam("fromDate") final int fromDate,
-								@QueryParam("toDate") final int toDate){
-		return Response.ok(ActivityRepo.getActivities()).build();
+	public Response getActivities(@DefaultValue("")@QueryParam("fromDate") final String fromDate,
+								  @DefaultValue("")@QueryParam("toDate") final String toDate){
+		if(fromDate.length() == 8 && toDate.length() == 8){
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+		    Date dateFrom;
+		    Date dateTo;
+			try {
+				dateFrom = sdf.parse(fromDate);
+				dateTo = sdf.parse(toDate);
+				return Response.ok(ActivityRepo.getActivities(dateFrom,dateTo)).build();
+			} catch (ParseException e) {
+				return Response.status(Status.BAD_REQUEST).build();
+			}
+		}
+		return Response.status(Status.BAD_REQUEST).build(); 
 	}
 	
 	@GET
