c002ec8a5e90563cb141f8106a3ec35659051a5c
Neeten <sandra.eliasson94@gmail.com>
2015-05-05 10:35:59 +0200

    börjat på class_info_view

--- Sats/app/src/main/java/sats/android/piedpiper/se/sats/MoreInfoActivity.java
+++ Sats/app/src/main/java/sats/android/piedpiper/se/sats/MoreInfoActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 
+import static sats.android.piedpiper.se.sats.R.layout.class_info_view;
 import static sats.android.piedpiper.se.sats.R.layout.my_training_listview;
 
 /**
@@ -14,7 +15,7 @@ public class MoreInfoActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(my_training_listview);
+        setContentView(class_info_view);
 
     }
 }
