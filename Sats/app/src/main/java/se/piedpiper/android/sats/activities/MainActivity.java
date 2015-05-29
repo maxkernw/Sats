01a32359d8e33682f095c00a08b5dc99cafe327c
OsamaB <osama.benhabara@live.se>
2015-05-29 18:22:50 +0200

    Change MainActivity to Max-version

--- Sats/app/src/main/java/se/piedpiper/android/sats/activities/MainActivity.java
+++ Sats/app/src/main/java/se/piedpiper/android/sats/activities/MainActivity.java
@@ -37,7 +37,7 @@ public final class MainActivity extends ActionBarActivity
 
     public static DateTime startTime = new DateTime().withDate(2013, 12, 29);
     public static DateTime today = new DateTime().withDate(2015, 4, 7);
-    
+
     public StickyListHeadersListView listView;
     private Date todaydate = today.toDate();
     private static android.app.Activity activity;
@@ -65,72 +65,9 @@ public final class MainActivity extends ActionBarActivity
         final RealmResults<Activity> realmActivities = realm.allObjects(Activity.class);
         final int realmSize = realmActivities.size();
 
-        if(realmSize == 0)
-        {
-            APIResponseHandler responseHandler = new APIResponseHandler(this);
-            realm.close();
-            responseHandler.getAllActivities(listView);
-        }
-        else
-        {
-            StorageHandler sh = new StorageHandler(activity);
-            sh.getAllActivities(listView);
-
-            for (Activity activity : realmActivities)
-            {
-                activitiesList.add(activity);
-            }
-
-            int x = activitiesList.size();
-            int y;
-            for (int m = x; m >= 0; m--) {
-                for (int i = 0; i < x - 1; i++) {
-                    y = i + 1;
-                    if (activitiesList.get(i).getDate().getTime() > activitiesList.get(y).getDate().getTime()) {
-                        Activity temp;
-                        temp = activitiesList.get(i);
-                        activitiesList.set(i, activitiesList.get(y));
-                        activitiesList.set(y, temp);
-                    }
-                }
-            }
-            CustomAdapter adapter = new CustomAdapter(activity, activitiesList);
-            listView.setAdapter(adapter);
-            while(adapter.getItemViewType())
-            {
-                realm.close();
-            }
-        }
-
-        leftMarker = new ImageView(activity);
-        leftMarker.setImageResource(R.drawable.back_to_now_right);
-        RelativeLayout rlLeft = (RelativeLayout) findViewById(R.id.mainRelative);
-        final RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(
-                RelativeLayout.LayoutParams.WRAP_CONTENT,
-                RelativeLayout.LayoutParams.WRAP_CONTENT);
-        lp.addRule(RelativeLayout.BELOW, R.id.toolbar);
-        lp.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
-        leftMarker.setScaleX(0.6f);
-        leftMarker.setScaleY(0.6f);
-        leftMarker.setX(26);
-        leftMarker.setY(-19);
-        leftMarker.setVisibility(View.INVISIBLE);
-        rlLeft.addView(leftMarker, lp);
-
-        rightMarker = new ImageView(activity);
-        rightMarker.setImageResource(R.drawable.back_to_now_left);
-        RelativeLayout rlRight = (RelativeLayout) findViewById(R.id.mainRelative);
-        final RelativeLayout.LayoutParams lp2 = new RelativeLayout.LayoutParams(
-                RelativeLayout.LayoutParams.WRAP_CONTENT,
-                RelativeLayout.LayoutParams.WRAP_CONTENT);
-        lp2.addRule(RelativeLayout.BELOW, R.id.toolbar);
-        lp2.addRule(RelativeLayout.ALIGN_PARENT_START);
-        rightMarker.setScaleX(0.6f);
-        rightMarker.setScaleY(0.6f);
-        rightMarker.setX(-20);
-        rightMarker.setY(-19);
-        rightMarker.setVisibility(View.INVISIBLE);
-        rlRight.addView(rightMarker, lp2);
+        checkRealm(realmSize, realmActivities, activitiesList);
+        createLeftMarker();
+        createRightMarker();
 
         final SlidingUpPanelLayout slide = (SlidingUpPanelLayout) findViewById(R.id.sliding_layout);
         graphAdapter = new ViewPagerAdapter(activity);
@@ -271,5 +208,81 @@ public final class MainActivity extends ActionBarActivity
                 overridePendingTransition(R.anim.next_activity, R.anim.open_activity);
             }
         });
+    }
+
+    private void checkRealm(int realmSize, RealmResults<Activity> realmActivities, ArrayList<Activity> activitiesList)
+    {
+        if(realmSize == 0)
+        {
+            APIResponseHandler responseHandler = new APIResponseHandler(this);
+            realm.close();
+            responseHandler.getAllActivities(listView);
+        }
+        else
+        {
+            StorageHandler sh = new StorageHandler(activity);
+            sh.getAllActivities(listView);
+
+            for (Activity activity : realmActivities)
+            {
+                activitiesList.add(activity);
+            }
+
+            int x = activitiesList.size();
+            int y;
+            for (int m = x; m >= 0; m--) {
+                for (int i = 0; i < x - 1; i++) {
+                    y = i + 1;
+                    if (activitiesList.get(i).getDate().getTime() > activitiesList.get(y).getDate().getTime()) {
+                        Activity temp;
+                        temp = activitiesList.get(i);
+                        activitiesList.set(i, activitiesList.get(y));
+                        activitiesList.set(y, temp);
+                    }
+                }
+            }
+            CustomAdapter adapter = new CustomAdapter(activity, activitiesList);
+            listView.setAdapter(adapter);
+            while(adapter.getItemViewType())
+            {
+                realm.close();
+            }
+        }
+    }
+
+    private void createRightMarker()
+    {
+        rightMarker = new ImageView(activity);
+        rightMarker.setImageResource(R.drawable.back_to_now_left);
+        RelativeLayout rlRight = (RelativeLayout) findViewById(R.id.mainRelative);
+        final RelativeLayout.LayoutParams lp2 = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT);
+        lp2.addRule(RelativeLayout.BELOW, R.id.toolbar);
+        lp2.addRule(RelativeLayout.ALIGN_PARENT_START);
+        rightMarker.setScaleX(0.6f);
+        rightMarker.setScaleY(0.6f);
+        rightMarker.setX(-20);
+        rightMarker.setY(-19);
+        rightMarker.setVisibility(View.INVISIBLE);
+        rlRight.addView(rightMarker, lp2);
+    }
+
+    private void createLeftMarker()
+    {
+        leftMarker = new ImageView(activity);
+        leftMarker.setImageResource(R.drawable.back_to_now_right);
+        RelativeLayout rlLeft = (RelativeLayout) findViewById(R.id.mainRelative);
+        final RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT);
+        lp.addRule(RelativeLayout.BELOW, R.id.toolbar);
+        lp.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+        leftMarker.setScaleX(0.6f);
+        leftMarker.setScaleY(0.6f);
+        leftMarker.setX(26);
+        leftMarker.setY(-19);
+        leftMarker.setVisibility(View.INVISIBLE);
+        rlLeft.addView(leftMarker, lp);
     }
 }
